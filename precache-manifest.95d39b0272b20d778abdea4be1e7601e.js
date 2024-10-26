30ad11858ba0228ffb1b5a58eb43a64466f666d0
Nelis Goeminne <5196255+NelisG@users.noreply.github.com>
2024-10-26 11:25:41 +0200

    Updates

---
+++ precache-manifest.95d39b0272b20d778abdea4be1e7601e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a335cad807e529753c5",
+    "url": "/SlimsteMensApp/static/css/main.65f47261.chunk.css"
+  },
+  {
+    "revision": "9a335cad807e529753c5",
+    "url": "/SlimsteMensApp/static/js/main.9a335cad.chunk.js"
+  },
+  {
+    "revision": "fbfa9ac1c04d6f818c50",
+    "url": "/SlimsteMensApp/static/js/1.fbfa9ac1.chunk.js"
+  },
+  {
+    "revision": "1d4a0da8683f8e4c88b7",
+    "url": "/SlimsteMensApp/static/js/runtime~main.1d4a0da8.js"
+  },
+  {
+    "revision": "f9da6d2caa55d5c4a2bec4ce1c9df73c",
+    "url": "/SlimsteMensApp/index.html"
+  }
+];
